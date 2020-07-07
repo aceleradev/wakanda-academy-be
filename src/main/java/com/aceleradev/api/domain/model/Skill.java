e642dff68a294dba316f3017738f28cd1b188cd2
Vinicius Reis <vinicius.reis@hdi.com.br>
2020-07-06 21:09:06 -0300

    implementa skill

--- src/main/java/com/aceleradev/api/domain/model/Skill.java
+++ src/main/java/com/aceleradev/api/domain/model/Skill.java
@@ -1,5 +1,83 @@
 package com.aceleradev.api.domain.model;
 
-public class Skill {
+import java.util.List;
 
+public class Skill {
+	private Long id;
+	private String code;
+	private String name;
+	private String description;
+	private Float difficulty;
+	private Tribe tribe;
+	private List<Lesson> lessons;
+	
+	public Skill() {
+	}
+
+	public Skill(Long id, String code, String name, String description, Float difficulty, Tribe tribe,
+			List<Lesson> lessons) {
+		this.id = id;
+		this.code = code;
+		this.name = name;
+		this.description = description;
+		this.difficulty = difficulty;
+		this.tribe = tribe;
+		this.lessons = lessons;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Float getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Float difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public Tribe getTribe() {
+		return tribe;
+	}
+
+	public void setTribe(Tribe tribe) {
+		this.tribe = tribe;
+	}
+
+	public List<Lesson> getLessons() {
+		return lessons;
+	}
+
+	public void setLessons(List<Lesson> lessons) {
+		this.lessons = lessons;
+	}
 }
