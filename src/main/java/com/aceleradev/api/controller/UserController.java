771f6c6c93b76f252a48472bdcabf588b5499cf0
Stenio <stenios@hdi.com.br>
2020-07-09 19:07:03 -0300

    alterações controller

--- src/main/java/com/aceleradev/api/controller/UserController.java
+++ src/main/java/com/aceleradev/api/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.aceleradev.api.controller.dto.UserCreatedDto;
