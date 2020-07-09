7046a707246f61f4f6472067a20bcd3f83909f0a
Vinicius Reis <vinicius.reis@hdi.com.br>
2020-07-09 20:50:09 -0300

    implementa controller

--- src/main/java/com/aceleradev/api/controller/WakanderController.java
+++ src/main/java/com/aceleradev/api/controller/WakanderController.java
@@ -3,11 +3,13 @@ package com.aceleradev.api.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.aceleradev.api.domain.model.Wakander;
 import com.aceleradev.api.service.WakandaService;
 
-@Controller
+@Controller("/wakander")
 public class WakanderController {
 	
 	private static final Logger log = LoggerFactory.getLogger(WakanderController.class);
@@ -17,11 +19,12 @@ public class WakanderController {
 	public WakanderController(WakandaService wakandaService) {
 		this.wakandaService = wakandaService;
 	}
-
-	public WakanderProfileDTO getWakanderProfile(String wankanderCode) {
+	
+	@GetMapping("/{wakanderCode}")
+	public WakanderProfileDTO getWakanderProfile(@PathVariable String wakanderCode) {
 		log.info("Start getWakanderProfile in WakanderController");
-		log.info("finding wakander by code: {}",wankanderCode);
-		Wakander wakander = wakandaService.findWakanderByCode(wankanderCode);
+		log.info("finding wakander by code: {}",wakanderCode);
+		Wakander wakander = wakandaService.findWakanderByCode(wakanderCode);
 		log.info("returning wakander: {}",wakander);
 		return new WakanderProfileDTO(wakander);
 	}
