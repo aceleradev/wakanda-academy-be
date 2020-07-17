eae433e61e6a938768ed4001f1645274f889d117
Vinicius Reis <vinicius.reis@hdi.com.br>
2020-07-17 12:56:11 -0300

    modifica estrategia de id

--- src/main/java/com/aceleradev/api/domain/model/Skill.java
+++ src/main/java/com/aceleradev/api/domain/model/Skill.java
@@ -18,7 +18,7 @@ import javax.persistence.Table;
 @Table(name = "skills")
 public class Skill {
 	
-	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(length = 60, unique = true)
