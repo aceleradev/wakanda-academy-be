fed48098917bffec759faf199eaea2dfb0d11334
Stenio <stenios@hdi.com.br>
2020-07-21 19:21:43 -0300

    ajuste add campos token

--- src/main/java/com/aceleradev/api/controller/dto/WakanderProfileDTO.java
+++ src/main/java/com/aceleradev/api/controller/dto/WakanderProfileDTO.java
@@ -3,12 +3,19 @@ package com.aceleradev.api.controller.dto;
 import com.aceleradev.api.domain.model.Wakander;
 
 public class WakanderProfileDTO {
+	
+	private String code;
 	private String nome;
 	private String email;
 
 	public WakanderProfileDTO(Wakander wakander) {
+		this.code = wakander.getCode();
 		this.nome = wakander.getName();
 		this.email = wakander.getEmail();
+	}
+	
+	public String getCode() {
+		return code;
 	}
 
 	public String getNome() {
