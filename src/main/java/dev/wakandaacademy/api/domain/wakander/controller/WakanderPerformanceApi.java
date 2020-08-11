eb4ade705d651c7170ce9aa84f5513624e4be230
Luara Novaes <luara.novaesss@gmail.com>
2020-08-11 18:09:35 -0300

    cria interface wakander performace

---
+++ src/main/java/dev/wakandaacademy/api/domain/wakander/controller/WakanderPerformanceApi.java
@@ -0,0 +1,8 @@
+package dev.wakandaacademy.api.domain.wakander.controller;
+
+import dev.wakandaacademy.api.domain.wakander.controller.dto.TotalTribesCompletedVSTotalTribesJourney;
+
+public interface WakanderPerformanceApi {
+	TotalTribesCompletedVSTotalTribesJourney getTotalTribesCompletedVSTotalTribesJourney(String WakanderCode);
+
+}
---
+++ src/main/java/dev/wakandaacademy/api/domain/wakander/controller/dto/TotalTribesCompletedVSTotalTribesJourney.java
@@ -0,0 +1,5 @@
+package dev.wakandaacademy.api.domain.wakander.controller.dto;
+
+public class TotalTribesCompletedVSTotalTribesJourney {
+
+}
