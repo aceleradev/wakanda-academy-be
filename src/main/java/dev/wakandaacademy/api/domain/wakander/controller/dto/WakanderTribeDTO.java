b4fe817ccf8240d817bfda2fe958dadeffab30e5
MirianLuz <61255081+MirianLuz@users.noreply.github.com>
2020-08-21 16:04:18 -0300

    Update WakanderTribeDTO.java

--- src/main/java/dev/wakandaacademy/api/domain/wakander/controller/dto/WakanderTribeDTO.java
+++ src/main/java/dev/wakandaacademy/api/domain/wakander/controller/dto/WakanderTribeDTO.java
@@ -1,10 +1,12 @@
 package dev.wakandaacademy.api.domain.wakander.controller.dto;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import dev.wakandaacademy.api.domain.wakander.model.Status;
 import dev.wakandaacademy.api.domain.wakander.model.WakanderTribe;
+import dev.wakandaacademy.api.domain.wakander.model.WakanderTribeInfo;
 
 public class WakanderTribeDTO {
 	private String tribeCode;
@@ -12,14 +14,15 @@ public class WakanderTribeDTO {
 	private String description;
 	private String iconUrl;
 	private Status status;
-	private Number completedPercentageTribe;
+	private BigDecimal completedPercentageTribe;
 
-	public WakanderTribeDTO(WakanderTribe wktribe) {
+	public WakanderTribeDTO(WakanderTribeInfo wktribe) {
 		this.tribeCode = wktribe.getTribe().getCode();
 		this.name = wktribe.getTribe().getName();
 		this.description = wktribe.getTribe().getDescription();
 		this.iconUrl = wktribe.getTribe().getIconURL();
 		this.status = wktribe.getStatus();
+		this.completedPercentageTribe=wktribe.getcompletedPercentageTribe();
 	}
 
 	public static List<WakanderTribeDTO> convert(List<WakanderTribe> wakanderTribe) {
