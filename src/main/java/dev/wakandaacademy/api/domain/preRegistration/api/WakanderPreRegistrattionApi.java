f1318cabc4d2b60b97143f853b12571ed690d6e3
Vinicius Reis <vinicius.reis@hdi.com.br>
2020-09-09 23:23:50 -0300

    implementa mock pre cadastro

---
+++ src/main/java/dev/wakandaacademy/api/domain/preRegistration/api/WakanderPreRegistrattionApi.java
@@ -0,0 +1,22 @@
+package dev.wakandaacademy.api.domain.preRegistration.api;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.wakandaacademy.api.domain.preRegistration.api.dto.WakanderPreRegistrationDetailDTO;
+import dev.wakandaacademy.api.domain.preRegistration.api.dto.form.WakanderPreReistrationForm;
+import dev.wakandaacademy.api.exception.ApiException;
+
+@RestController
+@RequestMapping("/public/wakander/pre-registrattion")
+public interface WakanderPreRegistrattionApi {
+	@PostMapping
+	@ResponseStatus(value = HttpStatus.CREATED)
+	WakanderPreRegistrationDetailDTO create(@RequestBody @Valid WakanderPreReistrationForm form) throws ApiException;
+}
