79449f7682fd6deefa95b90d3fe4ed063a25efa6
Stenio <stenios@hdi.com.br>
2020-10-01 22:17:05 -0300

    correção: criando implementação do calculador de previsão da jornada

---
+++ src/main/java/dev/wakandaacademy/api/domain/wakander/service/performace/impl/WakanderForecastJourneyDateCalculatorJpa.java
@@ -0,0 +1,19 @@
+package dev.wakandaacademy.api.domain.wakander.service.performace.impl;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Service;
+
+import dev.wakandaacademy.api.domain.wakander.model.Wakander;
+import dev.wakandaacademy.api.domain.wakander.service.performace.WakanderForecastJourneyDateCalculator;
+
+@Service
+public class WakanderForecastJourneyDateCalculatorJpa implements WakanderForecastJourneyDateCalculator {
+
+	@Override
+	public LocalDate calculateForecastJourney(Wakander wakander) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
